4211cb7897ed275e89567a2adaeafe49f97b98cd
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-12-29 17:52:58 +0900

    try new method_2

--- PanGestureExample/GridViewController.swift
+++ PanGestureExample/GridViewController.swift
@@ -2,15 +2,16 @@ import UIKit
 
 class GridViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    // MARK: - Properties
     let rectangleBox = UIView()
-
     var gridColumns: Int
     var gridRows: Int
     var boxes: [Bool]
-
+    let buttonOne = UIButton(type: .system)
+    let buttonTwo = UIButton(type: .system)
     var collectionView: UICollectionView!
 
-    // Custom initializer
+    // MARK: - Initializer
     init(rows: Int, columns: Int) {
         self.gridRows = rows
         self.gridColumns = columns
@@ -22,19 +23,37 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - UI Configuration
+    private func configureUI() {
         view.backgroundColor = .systemBackground
-        setupCollectionView()
         setupRectangleBox()
+        setupCollectionView()
+        setupButtons()
         setupConstraints()
     }
-    
+
     private func setupRectangleBox() {
-            rectangleBox.backgroundColor = .red
-            rectangleBox.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(rectangleBox)
-        }
+        rectangleBox.backgroundColor = .red
+        rectangleBox.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(rectangleBox)
+    }
+
+    private func setupButtons() {
+        configureButton(buttonOne, title: "One")
+        configureButton(buttonTwo, title: "Two")
+    }
+
+    private func configureButton(_ button: UIButton, title: String) {
+        button.setTitle(title, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+    }
 
     private func setupCollectionView() {
         let layout = UICollectionViewFlowLayout()
@@ -50,24 +69,30 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
 
     private func setupConstraints() {
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        let baseSpacing: CGFloat = 30
         let cellSize: CGFloat = 30
         let spacing: CGFloat = 2 // Assuming a spacing of 2 points between cells
         let totalCellWidth = (cellSize * CGFloat(gridColumns)) + (spacing * CGFloat(gridColumns - 1))
+        let totalCellheight = (cellSize * CGFloat(gridRows)) + (spacing * CGFloat(gridRows - 1))
 
         NSLayoutConstraint.activate([
-            collectionView.bottomAnchor.constraint(equalTo: rectangleBox.topAnchor, constant: baseSpacing),
-            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            collectionView.widthAnchor.constraint(equalToConstant: totalCellWidth),
-            
             rectangleBox.topAnchor.constraint(equalTo: view.topAnchor, constant: view.bounds.height * (3.5 / 7.0)),
             rectangleBox.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             rectangleBox.widthAnchor.constraint(equalToConstant: 100),
-            rectangleBox.heightAnchor.constraint(equalToConstant: 30)
+            rectangleBox.heightAnchor.constraint(equalToConstant: 30),
+            
+            collectionView.heightAnchor.constraint(equalToConstant: totalCellheight),
+            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            collectionView.widthAnchor.constraint(equalToConstant: totalCellWidth),
+            collectionView.bottomAnchor.constraint(equalTo: rectangleBox.topAnchor, constant: -20),
+            buttonOne.topAnchor.constraint(equalTo: rectangleBox.bottomAnchor, constant: 20),
+            buttonOne.leadingAnchor.constraint(equalTo: rectangleBox.leadingAnchor),
+
+            // Constraints for buttonTwo
+            buttonTwo.topAnchor.constraint(equalTo: rectangleBox.bottomAnchor, constant: 20),
+            buttonTwo.trailingAnchor.constraint(equalTo: rectangleBox.trailingAnchor),
+        
         ])
     }
-
-
 
 
     // UICollectionViewDataSource methods
@@ -86,7 +111,4 @@ class GridViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return CGSize(width: 30, height: 30)
     }
 
-
-
-    // Additional methods if needed...
 }
